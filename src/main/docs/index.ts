d50a8ef09a6ce28da948ba8001c947159f91ee4c
Layme, Reynaldo <layme.reynaldo@icloud.com>
2021-01-30 21:52:05 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Special API TS',
+    description: 'With NodeJs, TypeScript, TDD and special architecture clean, learning Udemy course of @rmanguinho',
+    version: '1.0.0'
+  }
+}
