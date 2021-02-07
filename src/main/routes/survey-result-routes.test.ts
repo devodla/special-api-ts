aa1fcbbd78c7912cf3b9822038bab370d9272541
Layme, Reynaldo <layme.reynaldo@icloud.com>
2021-02-07 13:24:33 -0300

    test: ensure load survey route returns 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -61,7 +61,8 @@ describe('Survey Result Routes', () => {
           image: 'http://image-name.com'
         }, {
           answer: ' Answer 2'
-        }]
+        }],
+        date: new Date()
       })
       const surveyId: string = res.ops[0]._id
       await request(app)
@@ -79,6 +80,25 @@ describe('Survey Result Routes', () => {
       await request(app)
         .get('/api/surveys/any_id/results')
         .expect(403)
+    })
+
+    test('Should return 200 on load survey result with accessToken', async () => {
+      const accessToken = await makeAccessToken()
+      const res = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1',
+          image: 'http://image-name.com'
+        }, {
+          answer: ' Answer 2'
+        }],
+        date: new Date()
+      })
+      const surveyId: string = res.ops[0]._id
+      await request(app)
+        .get(`/api/surveys/${surveyId}/results`)
+        .set('x-access-token', accessToken)
+        .expect(200)
     })
   })
 })
