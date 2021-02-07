7663fcb36efc6409b707c9a38202a24c12e7147b
Layme, Reynaldo <layme.reynaldo@icloud.com>
2021-02-07 13:32:53 -0300

    docs: add load survey result path

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -5,6 +5,7 @@ export const surveyResultPath = {
     }],
     tags: ['Enquete'],
     summary: 'API para criar a resposta de uma enquete',
+    description: 'Essa rota só pode ser executado por **usuários autenticados**',
     parameters: [
       {
         in: 'path',
@@ -24,6 +25,45 @@ export const surveyResultPath = {
         }
       }
     },
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  },
+  get: {
+    security: [{
+      apiKeyAuth: []
+    }],
+    tags: ['Enquete'],
+    summary: 'API para consultar o resultado de uma enquete',
+    description: 'Essa rota só pode ser executado por **usuários autenticados**',
+    parameters: [
+      {
+        in: 'path',
+        name: 'surveyId',
+        required: true,
+        schema: {
+          type: 'string'
+        }
+      }
+    ],
     responses: {
       200: {
         description: 'Sucesso',
