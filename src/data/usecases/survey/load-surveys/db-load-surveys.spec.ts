0736cc42956e5a995904e59f15b3ba92113b91ec
Layme, Reynaldo <layme.reynaldo@icloud.com>
2021-02-13 14:11:34 -0300

    test: change load surveys to add didAnswer field

--- src/data/usecases/survey/load-surveys/db-load-surveys.spec.ts
+++ src/data/usecases/survey/load-surveys/db-load-surveys.spec.ts
@@ -2,6 +2,7 @@ import { DbLoadSurveys } from './db-load-surveys'
 import { LoadSurveysRepositorySpy } from '@/data/test'
 import { throwError } from '@/domain/test'
 import MockDate from 'mockdate'
+import faker from 'faker'
 
 type SutTypes = {
   sut: DbLoadSurveys
@@ -28,13 +29,14 @@ describe('DbLoadSurveys', () => {
 
   test('Should call LoadSurveysRepository', async () => {
     const { sut, loadSurveysRepositorySpy } = makeSut()
-    await sut.load()
-    expect(loadSurveysRepositorySpy.callsCount).toBe(1)
+    const accountId = faker.random.uuid()
+    await sut.load(accountId)
+    expect(loadSurveysRepositorySpy.accountId).toBe(accountId)
   })
 
   test('Should return a list of Surveys on success', async () => {
     const { sut, loadSurveysRepositorySpy } = makeSut()
-    const surveys = await sut.load()
+    const surveys = await sut.load(faker.random.uuid())
     expect(surveys).toEqual(loadSurveysRepositorySpy.surveyModels)
   })
 
@@ -42,7 +44,7 @@ describe('DbLoadSurveys', () => {
     const { sut, loadSurveysRepositorySpy } = makeSut()
     jest.spyOn(loadSurveysRepositorySpy, 'loadAll')
       .mockImplementationOnce(throwError)
-    const promise = sut.load()
+    const promise = sut.load(faker.random.uuid())
     await expect(promise).rejects.toThrow()
   })
 })
